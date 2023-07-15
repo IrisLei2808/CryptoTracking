ceb0825203fe29b694871a9e5ff80e0b61fd4c1b
IrisLei2808 <duclt2808@gmail.com>
2023-07-15 16:20:56 -0700

    Initial Commit

---
+++ CryptoTracking/CryptoTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackingApp.swift
+//  CryptoTracking
+//
+//  Created by Duc Le on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
